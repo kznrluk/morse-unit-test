5d5e9e5c189ce0240e8d70443a7093ed089605b2
kqito <29191111+kqito@users.noreply.github.com>
2020-01-20 00:59:44 +0900

    Fix .eslintrc.json

--- src/libs/ConsoleOut/RealTimeResult.ts
+++ src/libs/ConsoleOut/RealTimeResult.ts
@@ -3,7 +3,7 @@ import { RealTimeResultStrings } from '../Result/RealTimeResultStrings';
 
 let outputRealTimeCount = 0;
 export const outputRealTimeResultString = (result: ResultCode) => {
-    process.stdout.write(RealTimeResultStrings[result] + ' ');
+    process.stdout.write(`${RealTimeResultStrings[result]} `);
     outputRealTimeCount += 1;
     if (outputRealTimeCount % 24 === 0) console.log();
 };
--- src/libs/config.ts
+++ src/libs/config.ts
@@ -2,11 +2,11 @@ export const VERSION = 'v0.0.1';
 export const OPENING_MESSAGE = `Thank you for using Morse Test Runner ${VERSION}`;
 
 export const CONSOLE_FORMAT = {
-    RED: '\u001b[91m',
-    BLUE: '\u001b[94m',
-    MAGENTA: '\u001b[95m',
-    RED_BG: '\u001b[41m',
-    BLUE_BG: '\u001b[44m',
-    MAGENTA_BG: '\u001b[45m',
-    END: '\u001b[00m'
-}
+    'RED': '\u001b[91m',
+    'BLUE': '\u001b[94m',
+    'MAGENTA': '\u001b[95m',
+    'RED_BG': '\u001b[41m',
+    'BLUE_BG': '\u001b[44m',
+    'MAGENTA_BG': '\u001b[45m',
+    'END': '\u001b[00m',
+};
