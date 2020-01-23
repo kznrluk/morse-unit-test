cba3bfce787c8d2301ffeddc9b61426fa1c13e62
kznrluk <kznr.luk@gmail.com>
2020-01-23 19:53:25 +0900

    Fix version

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const VERSION = 'v0.0.1';
-export const OPENING_MESSAGE = `Thank you for using Morse Test Runner ${VERSION}`;
+export const VERSION = '0.1.0';
+export const OPENING_MESSAGE = `Thank you for using Morse Test Runner v${VERSION}`;
